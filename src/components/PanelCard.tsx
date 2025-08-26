8cbd3f7866e5065072c1a95d00c0d4d0138586df
neo-deus <arpandesp7890@gmail.com>
2025-08-26 21:08:47 +0530

    linethrough added

--- src/components/PanelCard.tsx
+++ src/components/PanelCard.tsx
@@ -24,7 +24,13 @@ const PanelCard = ({ icon: Icon, title, subtodos }: TodoGroup) => {
               className=" w-5 h-5 rounded-full border-2 border-dashed border-neutral-500 data-[state=checked]:bg-violet-500 data-[state=checked]:border-none"
               defaultChecked={subtodo.status}
             />
-            <p className=" font-medium text-neutral-900">{subtodo.title}</p>
+            <p
+              className={`${
+                subtodo.status === true ? " line-through" : ""
+              } font-medium text-neutral-700 text-sm`}
+            >
+              {subtodo.title}
+            </p>
           </div>
           <div className=" flex items-center gap-3 font-medium">
             {subtodo.tags.map((tag, index) => (
